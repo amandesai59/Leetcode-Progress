091339db2037aad74613ee6aec97c3cdb0e7c0f1
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-07-10 19:30:20 +0530

    Time: 685 ms (38.94%), Space: 16.5 MB (99.29%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -5,21 +5,19 @@ class Solution:
             return -1
 
         n=len(grid)
-        m=len(grid[0])
-        if n==1 and m==1:
+        if n==1:
             return 1
-        heap=[]
-        heapq.heappush(heap, [1,[0,0]])
+        queue=deque([[1,0,0]])
 
-        while heap:
-            dist, [x,y]=heapq.heappop(heap)
+        while queue:
+            dist, x, y=queue.popleft()
             
             for i in range(-1,2):
                 for j in range(-1,2):
-                    if (i or j) and x+i<n and x+i>=0 and y+j>=0 and y+j<m and grid[x+i][y+j]==0:
-                        if x+i==n-1 and y+j==m-1:
+                    if (i or j) and x+i<n and x+i>=0 and y+j>=0 and y+j<n and grid[x+i][y+j]==0:
+                        if x+i==n-1 and y+j==n-1:
                             return dist+1
-                        heapq.heappush(heap, [dist+1,[x+i, y+j]])
+                        queue.append([dist+1, x+i, y+j])
                         grid[x+i][y+j]=1
 
         return -1
