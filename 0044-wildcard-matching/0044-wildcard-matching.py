6e33f9f88689f066e18ef4a0915d1ecedcbb1425
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-05 19:40:13 +0530

    Time: 455 ms (80.51%), Space: 16.3 MB (98.36%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -3,20 +3,24 @@ class Solution:
 
         n=len(s)
         m=len(p)
-        dp=[[False]*(m+1) for _ in range(n+1)]
-        dp[0][0]=True
+        prev = [False]*(m+1)
+        curr = [False]*(m+1)
+
+        prev[0]=True
         for j in range(m):
             if p[j]=='*':
-                dp[0][j+1]=True
+                prev[j+1]=True
             else:
                 break
 
         for i in range(1, n+1):
             for j in range(1, m+1):
-                ans=False
                 if s[i-1]==p[j-1] or p[j-1]=='?':
-                    dp[i][j] = dp[i-1][j-1]
+                    curr[j] = prev[j-1]
                 elif p[j-1]=='*':
-                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
+                    curr[j] = prev[j] or curr[j-1]
+                else:
+                    curr[j] = False
+            prev=curr[:]
 
-        return dp[n][m]
+        return prev[-1]
