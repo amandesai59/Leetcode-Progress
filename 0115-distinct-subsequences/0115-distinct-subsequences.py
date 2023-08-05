eaa08c5e3a49dc0ed5a58513580f4c29c0ae7be0
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-05 15:57:17 +0530

    Time: 288 ms (88.74%), Space: 16.5 MB (93.67%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -3,22 +3,24 @@ class Solution:
 
         n=len(s)
         m=len(t)
-        dp=[[0]*m for _ in range(n)]
+        prev = [0]*m
+        curr = [0]*m
         
         if s[0]==t[0]:
-            dp[0][0]=1
+            prev[0]=1
 
         for i in range(1,n):
             for j in range(m):
                 x=0
                 if s[i]==t[j]:
                     if j>0:
-                        x=dp[i-1][j-1]
+                        x=prev[j-1]
                     else:
                         x=1
 
-                y=dp[i-1][j]
+                y=prev[j]
 
-                dp[i][j]=x+y
+                curr[j]=x+y
+            prev=curr[:]
 
-        return dp[n-1][m-1]
+        return prev[m-1]
