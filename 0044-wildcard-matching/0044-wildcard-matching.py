9d9820176c323ada483bc5f2f9ac54665c0d9638
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-05 19:35:50 +0530

    Time: 537 ms (73.46%), Space: 24.7 MB (68.55%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -3,32 +3,20 @@ class Solution:
 
         n=len(s)
         m=len(p)
-        dp=[[-1]*m for _ in range(n)]
-        return self.rec(s,p,n-1, m-1, dp)
+        dp=[[False]*(m+1) for _ in range(n+1)]
+        dp[0][0]=True
+        for j in range(m):
+            if p[j]=='*':
+                dp[0][j+1]=True
+            else:
+                break
 
-    def rec(self, s, p, i, j, dp):
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                ans=False
+                if s[i-1]==p[j-1] or p[j-1]=='?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1]=='*':
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1]
 
-        if i<0:
-            while j>=0 and p[j]=='*':
-                j-=1
-            if j<0:
-                return True
-            return False
-
-        if j<0:
-            return False
-
-        if dp[i][j]!=-1:
-            return dp[i][j]
-
-        ans=False
-        if s[i]==p[j] or p[j]=='?':
-            ans = self.rec(s, p, i-1, j-1, dp)
-        elif p[j]=='*':
-            x = self.rec(s,p,i-1,j, dp)
-            y = self.rec(s,p,i-1,j-1, dp)
-            z = self.rec(s,p,i,j-1, dp)
-            ans = x or y or z
-        
-        dp[i][j] = ans
-        return ans
+        return dp[n][m]
