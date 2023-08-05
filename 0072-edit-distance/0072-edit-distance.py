b274125b6cf6524d81dec0d9e2ef86c595fb8ff5
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-05 19:00:39 +0530

    Time: 137 ms (66.88%), Space: 20 MB (44.9%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -4,30 +4,18 @@ class Solution:
         n=len(word1)
         m=len(word2)
 
-        dp = [[-1]*m for _ in range(n)]
-        return self.rec(word1, word2, n-1, m-1, dp)
+        dp = [[0]*(m+1) for _ in range(n+1)]
 
-    def rec(self, word1, word2, i, j, dp):
+        for i in range(n+1):
+            dp[i][0]=i
+        for j in range(m+1):
+            dp[0][j]=j
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])
 
-        if j<0:
-            return i+1
-
-        if i<0:
-            return j+1
-
-        if dp[i][j]!=-1:
-            return dp[i][j]
-
-        x=float('inf')
-        y=float('inf')
-        z=float('inf')
-
-        if word1[i]==word2[j]:
-            x=self.rec(word1, word2, i-1, j-1, dp)
-        else:
-            x=1+self.rec(word1, word2, i, j-1, dp)
-            y=1+self.rec(word1, word2, i-1, j-1, dp)
-            z=1+self.rec(word1, word2, i-1,j, dp)
-
-        dp[i][j]=min(x,y,z)
-        return dp[i][j]
+        return dp[n][m]
