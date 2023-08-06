cdfcc9e239722328e81e8bceedb6f2230f8c9a81
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-06 19:36:30 +0530

    Time: 1411 ms (36.07%), Space: 39.5 MB (30.1%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -2,23 +2,17 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
         n=len(prices)
-        dp=[[-1]*(4) for _ in range(n)]
-        return self.rec(prices, n, 0, 0, dp)
+        dp=[[0]*(5) for _ in range(n+1)]
 
-    def rec(self, prices, n, i, trNo, dp):
+        for i in range(n-1, -1, -1):
+            for j in range(3, -1,-1):
+                if j%2==0:
+                    x = - prices[i] + dp[i+1][j+1]
+                    y = dp[i+1][j]
+                else:
+                    x = prices[i] + dp[i+1][j+1]
+                    y = dp[i+1][j]
 
-        if i==n or trNo==4:
-            return 0
+                dp[i][j] = max(x,y)
 
-        if dp[i][trNo]!=-1:
-            return dp[i][trNo]
-
-        if trNo%2==0:
-            x = - prices[i] + self.rec(prices, n, i+1, trNo+1, dp)
-            y = self.rec(prices, n, i+1, trNo, dp)
-        else:
-            x = prices[i] + self.rec(prices, n, i+1, trNo+1, dp)
-            y = self.rec(prices, n, i+1, trNo, dp)
-
-        dp[i][trNo] = max(x,y)
-        return max(x,y)
+        return dp[0][0]
