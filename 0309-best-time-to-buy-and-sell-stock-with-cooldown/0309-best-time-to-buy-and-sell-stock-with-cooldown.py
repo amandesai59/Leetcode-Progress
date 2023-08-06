f8d927bd176e376a2114aacd0601e72a7a19a438
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-06 20:15:34 +0530

    Time: 51 ms (85.17%), Space: 16.7 MB (80.47%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,19 +2,21 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
         n=len(prices)
-        dp=[[[0]*2 for _ in range(2)] for _ in range(n+1)]
+        curr=[[0]*2 for _ in range(2)]
+        after=[[0]*2 for _ in range(2)]
 
         for i in range(n-1, -1, -1):
             for j in range(2):
-                dp[i][j][1] = dp[i+1][1][0]
+                curr[j][1] = after[1][0]
 
                 if j:
-                    x = -prices[i] + dp[i+1][0][0]
-                    y = dp[i+1][1][0]
+                    x = -prices[i] + after[0][0]
+                    y = after[1][0]
                 else:
-                    x = prices[i] + dp[i+1][1][1]
-                    y = dp[i+1][0][0]
+                    x = prices[i] + after[1][1]
+                    y = after[0][0]
 
-                dp[i][j][0]=max(x,y)
+                curr[j][0]=max(x,y)
+            after=curr[:]
 
-        return dp[0][1][0]
+        return curr[1][0]
