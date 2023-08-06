71d02f916c0005440518de86f8339ed350b162e5
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-06 19:21:10 +0530

    Time: 1846 ms (28.6%), Space: 56.7 MB (25.58%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -2,23 +2,18 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
         n=len(prices)
-        dp=[[[-1]*(3) for _ in range(2)] for _ in range(n)]
-        return self.rec(prices, n, 0, 2, 1, dp)
+        dp=[[[0]*(3) for _ in range(2)] for _ in range(n+1)]
 
-    def rec(self, prices, n, i, cap, buy, dp):
+        for i in range(n-1, -1, -1):
+            for j in range(2):
+                for k in range(1,3):
+                    if j:
+                        x = - prices[i] + dp[i+1][0][k]
+                        y = dp[i+1][1][k]
+                    else:
+                        x = prices[i] + dp[i+1][1][k-1]
+                        y = dp[i+1][0][k]
 
-        if i==n or cap==0:
-            return 0
+                    dp[i][j][k] = max(x,y)
 
-        if dp[i][buy][cap]!=-1:
-            return dp[i][buy][cap]
-
-        if buy:
-            x = - prices[i] + self.rec(prices, n, i+1, cap, 0, dp)
-            y = self.rec(prices, n, i+1, cap, 1, dp)
-        else:
-            x = prices[i] + self.rec(prices, n, i+1, cap-1, 1, dp)
-            y = self.rec(prices, n, i+1, cap, 0, dp)
-
-        dp[i][buy][cap] = max(x,y)
-        return max(x,y)
+        return dp[0][1][2]
