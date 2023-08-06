ccb871a6a966ba8db6f16b531e6a332ca6757595
Aman Desai <68956904+amandesai59@users.noreply.github.com>
2023-08-06 22:32:59 +0530

    Time: 725 ms (47%), Space: 25.7 MB (21.66%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,23 +2,11 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
 
         n=len(prices)
-        dp=[[-1]*2 for _ in range(n)]
-        return self.rec(prices, n, 0, 1, fee, dp)
-    
-    def rec(self, prices, n, i, buy, fee, dp):
+        dp=[[0]*2 for _ in range(n+1)]
+        
+        for i in range(n-1, -1, -1):
+            
+            dp[i][1] = max(-prices[i] + dp[i+1][0], dp[i+1][1])
+            dp[i][0] = max(prices[i] -fee + dp[i+1][1], dp[i+1][0])
 
-        if i==n:
-            return 0
-
-        if dp[i][buy]!=-1:
-            return dp[i][buy]
-
-        if buy:
-            x = -prices[i] + self.rec(prices, n, i+1, 0, fee, dp)
-            y = self.rec(prices, n, i+1, 1, fee, dp)
-        else:
-            x = prices[i] - fee + self.rec(prices, n, i+1, 1, fee ,dp)
-            y = self.rec(prices, n, i+1, 0, fee, dp)
-
-        dp[i][buy] = max(x,y)
-        return dp[i][buy]
+        return dp[0][1]
